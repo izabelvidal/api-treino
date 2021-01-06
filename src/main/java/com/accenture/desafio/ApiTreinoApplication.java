57b6c4ecf97f498b528933020cb297eb582bd9c3
Izabel Vidal <izabelvidal99@gmail.com>
2021-01-06 10:10:52 -0300

    first commit

---
+++ src/main/java/com/accenture/desafio/ApiTreinoApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTreinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTreinoApplication.class, args);
+	}
+
+}
